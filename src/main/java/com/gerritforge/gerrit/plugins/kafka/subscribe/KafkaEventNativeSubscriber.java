90db8ef92e2e2002b55cdb61e4cbda40362ada76
Antonio Barone <syntonyze@gmail.com>
2026-02-06 17:45:04 +0100

    Implement the new acknowledgement-aware consumer API
    When a consumer uses the acknowledgement-aware API, calling
    acknowledgement.ack() triggers an immediate synchronous offset commit on
    the underlying Kafka consumer.
    
    This allows Gerrit event consumers to have fine-grained control over
    message acknowledgment when auto-commit is disabled.
    
    Depends-On: https://gerrit-review.googlesource.com/c/modules/events-broker/+/549941
    Bug: Issue 486866989
    Change-Id: I5b7630b174d0b91d5b9fb90b23550449aa4b44d6

--- src/main/java/com/gerritforge/gerrit/plugins/kafka/subscribe/KafkaEventNativeSubscriber.java
+++ src/main/java/com/gerritforge/gerrit/plugins/kafka/subscribe/KafkaEventNativeSubscriber.java
@@ -12,6 +12,7 @@ package com.gerritforge.gerrit.plugins.kafka.subscribe;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.gerritforge.gerrit.eventbroker.AckAwareConsumer;
 import com.gerritforge.gerrit.plugins.kafka.broker.ConsumerExecutor;
 import com.gerritforge.gerrit.plugins.kafka.config.KafkaSubscriberProperties;
 import com.google.common.flogger.FluentLogger;
@@ -45,7 +46,7 @@ public class KafkaEventNativeSubscriber implements KafkaEventSubscriber {
   private final KafkaConsumerFactory consumerFactory;
   private final Deserializer<byte[]> keyDeserializer;
 
-  private java.util.function.Consumer<Event> messageProcessor;
+  private AckAwareConsumer<Event> messageProcessor;
   private String topic;
   private AtomicBoolean resetOffset = new AtomicBoolean(false);
 
@@ -74,13 +75,10 @@ public class KafkaEventNativeSubscriber implements KafkaEventSubscriber {
     externalGroupId.ifPresent(gid -> this.configuration.setProperty("group.id", gid));
   }
 
-  /* (non-Javadoc)
-   * @see com.gerritforge.gerrit.plugins.kafka.subscribe.KafkaEventSubscriber#subscribe(java.lang.String, java.util.function.Consumer)
-   */
   @Override
-  public void subscribe(String topic, java.util.function.Consumer<Event> messageProcessor) {
+  public void subscribe(String topic, AckAwareConsumer<Event> acknowledgementConsumer) {
     this.topic = topic;
-    this.messageProcessor = messageProcessor;
+    this.messageProcessor = acknowledgementConsumer;
     logger.atInfo().log(
         "Kafka consumer subscribing to topic alias [%s] for event topic [%s] with groupId [%s]",
         topic, topic, configuration.getGroupId());
@@ -113,7 +111,7 @@ public class KafkaEventNativeSubscriber implements KafkaEventSubscriber {
    * @see com.gerritforge.gerrit.plugins.kafka.subscribe.KafkaEventSubscriber#getMessageProcessor()
    */
   @Override
-  public java.util.function.Consumer<Event> getMessageProcessor() {
+  public AckAwareConsumer<Event> getMessageProcessor() {
     return messageProcessor;
   }
 
@@ -177,7 +175,11 @@ public class KafkaEventNativeSubscriber implements KafkaEventSubscriber {
                 try (ManualRequestContext ctx = oneOffCtx.open()) {
                   Event event =
                       valueDeserializer.deserialize(consumerRecord.topic(), consumerRecord.value());
-                  messageProcessor.accept(event);
+                  messageProcessor.accept(
+                      event,
+                      configuration.isAutoCommitEnabled()
+                          ? KafkaAutoAcknowledgement.INSTANCE
+                          : new KafkaExplicitAcknowledgement(consumerRecord, consumer));
                 } catch (Exception e) {
                   logger.atSevere().withCause(e).log(
                       "Malformed event '%s': [Exception: %s]",
