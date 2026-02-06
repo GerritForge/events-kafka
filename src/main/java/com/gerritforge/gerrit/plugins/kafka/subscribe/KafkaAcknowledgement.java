68816d310b9e890fba78054705b8311f116a02ca
Antonio Barone <syntonyze@gmail.com>
2026-02-06 17:45:04 +0100

    Implement the new acknowledgement-aware consumer API
    When a consumer uses the acknowledgement-aware API, calling
    acknowledgement.ack() triggers an immediate synchronous offset commit on
    the underlying Kafka consumer.
    
    This allows Gerrit event consumers to have fine-grained control over
    message acknowledgment when auto-commit is disabled.
    
    Depends-On: https://gerrit-review.googlesource.com/c/modules/events-broker/+/549941
    Bug: Issue 486866989
    Change-Id: I5b7630b174d0b91d5b9fb90b23550449aa4b44d6

---
+++ src/main/java/com/gerritforge/gerrit/plugins/kafka/subscribe/KafkaAcknowledgement.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.kafka.subscribe;
+
+import com.gerritforge.gerrit.eventbroker.MessageAcknowledgement;
+import com.gerritforge.gerrit.eventbroker.MessageAcknowledgementException;
+import com.google.common.flogger.FluentLogger;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.TopicPartition;
+
+class KafkaAcknowledgement implements MessageAcknowledgement {
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
+
+  private final ConsumerRecord<byte[], byte[]> consumerRecord;
+  private final Consumer<byte[], byte[]> consumer;
+  private final AtomicBoolean acked = new AtomicBoolean(false);
+
+  KafkaAcknowledgement(
+      ConsumerRecord<byte[], byte[]> consumerRecord, Consumer<byte[], byte[]> consumer) {
+    this.consumerRecord = consumerRecord;
+    this.consumer = consumer;
+  }
+
+  @Override
+  public void ack() {
+    if (acked.compareAndSet(false, true)) {
+      TopicPartition tp = new TopicPartition(consumerRecord.topic(), consumerRecord.partition());
+      long offset = consumerRecord.offset() + 1;
+      try {
+        consumer.commitSync(Map.of(tp, new OffsetAndMetadata(offset)));
+      } catch (KafkaException e) {
+        throw new MessageAcknowledgementException(
+            String.format("Failed to acknowledge offset %d on %s", offset, tp), e);
+      }
+      log.atWarning().log("Committed offset %d on %s", offset, tp);
+    }
+  }
+
+  @Override
+  public boolean isAutoAck() {
+    return false;
+  }
+}
