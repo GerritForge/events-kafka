42468b1735d2c2e813f3cf80035a37d6cea55565
Antonio Barone <syntonyze@gmail.com>
2026-02-06 17:45:04 +0100

    [DO NOT SUBMIT] Review suggestions testing
    Testing if the reviews suggested at [1] make sense or not.
    
    [1] https://review.gerrithub.io/c/GerritForge/events-kafka/+/1230792
    
    Depends-On: https://gerrit-review.googlesource.com/c/modules/events-broker/+/578961
    Bug: Issue 486866989
    Change-Id: I3734e60d4818cb32880648d0d9abd69dcf96c8d3

--- src/main/java/com/gerritforge/gerrit/plugins/kafka/config/KafkaSubscriberProperties.java
+++ src/main/java/com/gerritforge/gerrit/plugins/kafka/config/KafkaSubscriberProperties.java
@@ -12,20 +12,25 @@
 package com.gerritforge.gerrit.plugins.kafka.config;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 @Singleton
 public class KafkaSubscriberProperties extends KafkaProperties {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private static final long serialVersionUID = 1L;
   public static final String DEFAULT_POLLING_INTERVAL_MS = "1000";
   public static final String DEFAULT_NUMBER_OF_SUBSCRIBERS = "7";
+  private static final String DEFAULT_ENABLE_AUTO_COMMIT = "true";
 
   private final Integer pollingInterval;
   private final String groupId;
   private final Integer numberOfSubscribers;
+  private final boolean autoCommitEnabled;
 
   @Inject
   public KafkaSubscriberProperties(
@@ -37,12 +42,24 @@ public class KafkaSubscriberProperties extends KafkaProperties {
     this.groupId = getProperty("group.id");
     this.numberOfSubscribers =
         Integer.parseInt(getProperty("number.of.subscribers", DEFAULT_NUMBER_OF_SUBSCRIBERS));
+    this.autoCommitEnabled =
+        Boolean.parseBoolean(
+            getProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, DEFAULT_ENABLE_AUTO_COMMIT));
+    warnIfManualAckIsConfiguredWithRestClient();
   }
 
   @VisibleForTesting
   public KafkaSubscriberProperties(
       int pollingInterval, String groupId, int numberOfSubscribers, ClientType clientType) {
-    this(pollingInterval, groupId, numberOfSubscribers, clientType, null, null, null);
+    this(
+        pollingInterval,
+        groupId,
+        numberOfSubscribers,
+        clientType,
+        /* autoCommit */ true,
+        null,
+        null,
+        null);
   }
 
   @VisibleForTesting
@@ -51,6 +68,7 @@ public class KafkaSubscriberProperties extends KafkaProperties {
       String groupId,
       int numberOfSubscribers,
       ClientType clientType,
+      boolean autoCommitEnabled,
       String restApiUriString,
       String restApiUsername,
       String restApiPassword) {
@@ -58,6 +76,9 @@ public class KafkaSubscriberProperties extends KafkaProperties {
     this.pollingInterval = pollingInterval;
     this.groupId = groupId;
     this.numberOfSubscribers = numberOfSubscribers;
+    this.autoCommitEnabled = autoCommitEnabled;
+    setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, Boolean.toString(autoCommitEnabled));
+    warnIfManualAckIsConfiguredWithRestClient();
   }
 
   public Integer getPollingInterval() {
@@ -70,5 +91,18 @@ public class KafkaSubscriberProperties extends KafkaProperties {
 
   public Integer getNumberOfSubscribers() {
     return numberOfSubscribers;
+  }
+
+  public boolean isAutoCommitEnabled() {
+    return autoCommitEnabled;
+  }
+
+  private void warnIfManualAckIsConfiguredWithRestClient() {
+    if (getClientType() == ClientType.REST && !autoCommitEnabled) {
+      logger.atWarning().log(
+          "plugin.events-kafka.enableAutoCommit=false is ignored when clientType=REST; "
+              + "the REST consumer always acknowledges messages automatically. "
+              + "Use clientType=NATIVE for explicit acknowledgements.");
+    }
   }
 }
