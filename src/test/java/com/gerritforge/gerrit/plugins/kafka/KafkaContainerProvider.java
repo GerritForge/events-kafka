e41e4a207d401bfa2fb48908d69e8e3a02bcb8df
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-04 18:40:21 +0100

    Fix Kafka TestContainer startup on MacOS
    On MacOS with fast M4 or later processors, the TestContainer execution
    can be faster than Docker container port mapping and it could result in
    a stale in-memory map that would make all IT tests fail.
    
    Introduce a 10s wait loop where the fetching of the port binding is retried
    and the container startup won't fail randomly anymore.
    
    Change-Id: I3ab44cf346db408bfe3340957696b0901d2822fe

--- src/test/java/com/gerritforge/gerrit/plugins/kafka/KafkaContainerProvider.java
+++ src/test/java/com/gerritforge/gerrit/plugins/kafka/KafkaContainerProvider.java
@@ -11,6 +11,11 @@
 
 package com.gerritforge.gerrit.plugins.kafka;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Ports;
+import com.google.common.base.Preconditions;
+import com.google.common.flogger.FluentLogger;
 import java.util.Map;
 import org.junit.Ignore;
 import org.testcontainers.containers.KafkaContainer;
@@ -22,6 +27,8 @@ public class KafkaContainerProvider {
   public static int KAFKA_PORT_INTERNAL = KafkaContainer.KAFKA_PORT + 1;
   private static final String KAFKA_IMAGE_NAME = "confluentinc/cp-kafka";
   private static final String KAFKA_IMAGE_TAG = "5.4.3";
+  private static final int WAIT_FOR_PORT_BINDING_SEC = 10;
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
 
   public static KafkaContainer get() {
     KafkaContainer kafkaContainer =
@@ -32,6 +39,62 @@ public class KafkaContainerProvider {
             return String.format(
                     "INTERNAL://%s:%s,", getNetworkAliases().get(0), KAFKA_PORT_INTERNAL)
                 + super.getBootstrapServers();
+          }
+
+          @Override
+          public Integer getMappedPort(int originalPort) {
+            int waitPatienceSec = WAIT_FOR_PORT_BINDING_SEC;
+            Integer mappedPort = null;
+
+            while (mappedPort == null && waitPatienceSec > 0) {
+              mappedPort = getMappedPortFromContainer(originalPort);
+              if (mappedPort == null) {
+                try {
+                  Thread.sleep(1000L);
+                } catch (InterruptedException ex) {
+                  throw new RuntimeException(ex);
+                }
+              }
+              waitPatienceSec--;
+            }
+            if (mappedPort == null) {
+              throw new IllegalArgumentException(
+                  String.format(
+                      "Unable to get mapped port %d from container %s after %d seconds",
+                      originalPort, getContainerId(), WAIT_FOR_PORT_BINDING_SEC));
+            }
+            return mappedPort;
+          }
+
+          private Integer getMappedPortFromContainer(int originalPort) {
+            String containerId = this.getContainerId();
+            Preconditions.checkState(
+                this.getContainerId() != null,
+                "Mapped port can only be obtained after the container is started");
+
+            Ports.Binding[] binding = getBindings(containerId, originalPort);
+            if (binding != null && binding.length > 0 && binding[0] != null) {
+              return Integer.valueOf(binding[0].getHostPortSpec());
+            } else {
+              log.atWarning().log(
+                  "Unable to get Kafka mapped port %d from container %s",
+                  originalPort, containerId);
+              return null;
+            }
+          }
+
+          private Ports.Binding[] getBindings(String containerId, int originalPort) {
+            Map<ExposedPort, Ports.Binding[]> bindings = getBindings(getContainerInfo());
+            if (bindings == null || bindings.isEmpty()) {
+              bindings = getBindings(dockerClient.inspectContainerCmd(containerId).exec());
+            }
+
+            return bindings.get(new ExposedPort(originalPort));
+          }
+
+          private static Map<ExposedPort, Ports.Binding[]> getBindings(
+              InspectContainerResponse containerInfo) {
+            return containerInfo.getNetworkSettings().getPorts().getBindings();
           }
         };
 
