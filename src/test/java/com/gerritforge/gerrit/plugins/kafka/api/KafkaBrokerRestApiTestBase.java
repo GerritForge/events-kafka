6eea0c0239caf007a530382c4ae0f5547356949d
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-04 18:40:21 +0100

    Fix Kafka TestContainer startup on MacOS
    On MacOS with fast M4 or later processors, the TestContainer execution
    can be faster than Docker container port mapping and it could result in
    a stale in-memory map that would make all IT tests fail.
    
    Introduce a 10s wait loop where the fetching of the port binding is
    retried and the container startup won't fail randomly anymore.
    
    Change-Id: I3ab44cf346db408bfe3340957696b0901d2822fe

--- src/test/java/com/gerritforge/gerrit/plugins/kafka/api/KafkaBrokerRestApiTestBase.java
+++ src/test/java/com/gerritforge/gerrit/plugins/kafka/api/KafkaBrokerRestApiTestBase.java
@@ -10,10 +10,10 @@
 // limitations under the License.
 package com.gerritforge.gerrit.plugins.kafka.api;
 
+import com.gerritforge.gerrit.plugins.kafka.GenericContainerWithPortMappingFix;
 import com.gerritforge.gerrit.plugins.kafka.KafkaRestContainer;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
@@ -45,7 +45,7 @@ public class KafkaBrokerRestApiTestBase extends KafkaBrokerApiTest {
             kafkaRestWithId.getKafkaRestContainerIP(),
             KafkaRestContainer.KAFKA_REST_PORT);
     nginx =
-        new GenericContainer<>(
+        new GenericContainerWithPortMappingFix<>(
                 new ImageFromDockerfile()
                     .withDockerfileFromBuilder(
                         builder ->
