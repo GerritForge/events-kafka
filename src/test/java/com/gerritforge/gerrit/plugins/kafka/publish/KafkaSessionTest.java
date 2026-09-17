9ca9d4a5139173e56445ff09a0c937e061504b74
Luca Milanesio <luca.milanesio@gmail.com>
2026-09-17 07:18:15 +0100

    Support the requeue() method in BrokerApi
    Implement the support for the requeue() method for propagating the
    REQUEUE direction to the message listener.
    
    The support of requeue() helps the message listeners to differentiate
    between the initial delivery of messages and the resend to the messaging
    system because of retries.
    
    Change-Id: I72dbcae61ffed27c71a31c93bd7b74ffa071f0cd

--- src/test/java/com/gerritforge/gerrit/plugins/kafka/publish/KafkaSessionTest.java
+++ src/test/java/com/gerritforge/gerrit/plugins/kafka/publish/KafkaSessionTest.java
@@ -104,7 +104,8 @@ public class KafkaSessionTest {
     when(kafkaProducer.send(any())).thenReturn(Futures.immediateFuture(recordMetadata));
     connectWithPartition();
 
-    objectUnderTest.publish(topic, Optional.of(PARTITION), message);
+    objectUnderTest.publish(
+        topic, Optional.of(PARTITION), message, MessageLogger.Direction.PUBLISH);
 
     verify(kafkaProducer).send(recordCaptor.capture());
     assertThat(recordCaptor.getValue().partition()).isEqualTo(PARTITION);
@@ -116,7 +117,8 @@ public class KafkaSessionTest {
     when(kafkaProducer.send(any(), any())).thenReturn(Futures.immediateFuture(recordMetadata));
     connectWithPartition();
 
-    objectUnderTest.publish(topic, Optional.of(PARTITION), message);
+    objectUnderTest.publish(
+        topic, Optional.of(PARTITION), message, MessageLogger.Direction.PUBLISH);
 
     verify(kafkaProducer).send(recordCaptor.capture(), any());
     assertThat(recordCaptor.getValue().partition()).isEqualTo(PARTITION);
@@ -130,7 +132,9 @@ public class KafkaSessionTest {
     IllegalArgumentException thrown =
         assertThrows(
             IllegalArgumentException.class,
-            () -> objectUnderTest.publish(topic, Optional.of(PARTITION), message));
+            () ->
+                objectUnderTest.publish(
+                    topic, Optional.of(PARTITION), message, MessageLogger.Direction.PUBLISH));
 
     assertThat(thrown).hasMessageThat().isEqualTo(String.format(PARTITION_ERROR, PARTITION, topic));
   }
@@ -140,8 +144,10 @@ public class KafkaSessionTest {
     when(kafkaProducer.send(any())).thenReturn(Futures.immediateFuture(recordMetadata));
     connectWithPartition();
 
-    objectUnderTest.publish(topic, Optional.of(PARTITION), message);
-    objectUnderTest.publish(topic, Optional.of(PARTITION), message);
+    objectUnderTest.publish(
+        topic, Optional.of(PARTITION), message, MessageLogger.Direction.PUBLISH);
+    objectUnderTest.publish(
+        topic, Optional.of(PARTITION), message, MessageLogger.Direction.PUBLISH);
 
     verify(kafkaProducer, times(1)).partitionsFor(topic);
   }
